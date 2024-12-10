f44298a95117f6997b914e421ef962bd45634d52
Se Jun Park <20203110@office.deu.ac.kr>
2024-12-10 15:09:14 +0900

    Login/Signup (#1)

---
+++ AdminMainMenu.h
@@ -0,0 +1,13 @@
+#pragma once
+class AdminMainMenu
+{
+private:
+	static const int BOOKMENU = 5;
+	static const int SALES = 7;
+	static const int INOUT = 9;
+	static const int ADDADMIN = 11;
+	static const int LOGOUT = 13;
+public:
+	void printSrc();
+};
+
--- MainMenu.h
+++ MainMenu.h
@@ -2,10 +2,9 @@
 
 class MainMenu {
 private:
-	static const int USERLOGIN = 5;
+	static const int LOGIN = 5;
 	static const int SIGNUP = 7;
-	static const int ADMINLOGIN = 9;
-	static const int EXIT = 11;
+	static const int EXIT = 9;
 public:
 	void printSrc();
 };
